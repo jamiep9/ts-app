bad8a6992a74a794289b527a5f17da58a7e3ca22
jamiep9 <jep349@cornell.edu>
2023-10-02 21:56:17 -0400

    finished run page

--- src/components/FunRunning.tsx
+++ src/components/FunRunning.tsx
@@ -44,22 +44,24 @@ export const FunRunning: React.FC = () => {
                             SHAPE + Health Women's Half
                         </h2>
                         <h2 id='float2'>
-                            In April, 
+                            In April, I ran 2 laps around Central Park with 7,000 other women! It was my best half marathon
+                            time to-date despite it raining most of the race. I finished the race exhausted and ready to eat
+                            a bagel!
                         </h2>
                     </Col>
                 </Row>
                 <Row gutter={50} style={{marginBottom: '50px'}}>
-                    <Col span={9} className='gutter-row'>
+                    <Col span={8} className='gutter-row'>
                         <h2 id='float' className='sectionH2'>
-                            Denver
+                            Fred Lebow Half Marathon
                         </h2>
                         <h2 id='float2'>
-                            I went on a solo trip to Denver, Colorado in September 2023 and it was one of the highlights of this year!
-                            I really enjoyed the challenging hikes and the beautiful views. One of my favorite hikes was the Walker Ranch Loop,
-                            an 8-mile loop just outside of Boulder. Although I got winded at times, it was worth it for the incredible mountain views!
+                            I ran my first-ever half marathon on January 23! It was exhausting but worth the run. Hopefully next half
+                            I don't end up walking the last four miles. I discovered though that my favorite part of running is the signs that people hold along
+                            the sidelines!
                         </h2>
                     </Col>
-                    <Col span={5} className='gutter-row'>
+                    <Col span={6} className='gutter-row'>
                         <Image
                             id='float'
                             src={running3}
@@ -68,21 +70,21 @@ export const FunRunning: React.FC = () => {
                     </Col>
                 </Row>
                 <Row gutter={50} style={{marginBottom: '50px'}}>
-                    <Col span={5} className='gutter-row'>
+                    <Col span={6} className='gutter-row'>
                         <Image
                             id='float'
                             src={running4}
                             className='profileImage'
                         />
                     </Col>
-                    <Col span={9} className='gutter-row'>
+                    <Col span={8} className='gutter-row'>
                         <h2 id='float' className='sectionH2' style={{textAlign: 'right'}}>
-                            Denver
+                            New Years Midnight 4-Mile Run
                         </h2>
                         <h2 id='float2'>
-                            I went on a solo trip to Denver, Colorado in September 2023 and it was one of the highlights of this year!
-                            I really enjoyed the challenging hikes and the beautiful views. One of my favorite hikes was the Walker Ranch Loop,
-                            an 8-mile loop just outside of Boulder. Although I got winded at times, it was worth it for the incredible mountain views!
+                            To kick off 2023, a couple friends and I ran the New Years' Midnight Run in Central Park. The race kicked off
+                            with fireworks, which kept going off for the entire run! It was a bit chaotic but we stuck together and took 
+                            photos at every mile we completed!
                         </h2>
                     </Col>
                 </Row>
